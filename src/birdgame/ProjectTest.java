dfbd3ad0646ff19a5004d457482914d9dccbcb45
Matt Ward <mattward@udel.edu>
2019-04-18 14:08:28 -0400

    Updated tests

--- src/birdgame/ProjectTest.java
+++ src/birdgame/ProjectTest.java
@@ -27,11 +27,9 @@ public class ProjectTest {
 		p.width = 10;
 		p.xLocation = 1;
 		p.yLocation = 1;
-		Enemy e = new Enemy(0, 0);
+		Enemy e = new Enemy(1,1);
 		e.height = 5;
 		e.width = 5;
-		e.xLocation = 1;
-		e.yLocation = 1;
 		assert true == p.checkCollision(e);
 	}
 	@Test
@@ -41,11 +39,9 @@ public class ProjectTest {
 		p.width = 10;
 		p.xLocation = 1;
 		p.yLocation = 1;
-		Enemy e = new Enemy(0, 0);
+		Enemy e = new Enemy(500,500);
 		e.height = 5;
 		e.width = 5;
-		e.xLocation = 500;
-		e.yLocation = 500;
 		assert false == p.checkCollision(e);
 	}
 	
@@ -83,5 +79,70 @@ public class ProjectTest {
 		//Cannot write JUnit tests for paint(), update(), or displayX() methods due to visual nature of method
 	
 	//Tests for Model
-		//
+		//updateLocationAndDirection()
+	@Test
+	public void playerThatMovesPlus5XMovesPlus5X() {
+		Model m = new Model(1,1,1,1);
+		Player p = new Player();
+		m.gamePieces.add(p);
+		p.xLocation = 1;
+		p.xincr = 5;
+		m.updateLocationAndDirection();
+		assert p.xLocation == 6;
+	}
+	@Test
+	public void playerThatDoesNotMoveStaysInTheSameSpot() {
+		Model m = new Model(1,1,1,1);
+		Player p = new Player();
+		m.gamePieces.add(p);
+		p.xLocation = 1;
+		p.xincr = 0;
+		m.updateLocationAndDirection();
+		assert p.xLocation == 1;
+	}
+		//Cannot write JUnit tests for handleTicks() because it deals with screen updates (Calls methods in view)
+		//updateObstacles()
+	/*
+	@Test
+	public void obstacleThatMovesPlus5XMovesPlus5X() {
+		Model m = new Model(1,1,1,1);
+		Enemy e = new Enemy(1,1);
+		m.gamePieces.add(e);
+		e.xincr = 5;
+		m.updateObstacles();
+		assert e.xLocation == 6;
+	}
+	@Test
+	public void obstaceThatDoesNotMoveStaysInTheSameSpot() {
+		Model m = new Model(1,1,1,1);
+		Enemy e = new Enemy();
+		m.gamePieces[0] = e;
+		e.xLocation = 1;
+		e.xincr = 0;
+		m.updateObstacles();
+		assert e.xLocation == 1;
+	}*/
+		//Cannot write tests for spawnObstacle() due to random elements of spawning an object
+		//eat()
+	@Test
+	public void playerEatingIncreasesFoodScoreBy1() {
+		Model m = new Model(1,1,1,1);
+		Player p = new Player();
+		p.score = 0;
+		m.gamePieces.add(p);
+		Food f = new Food(1,1);
+		m.eat(f);
+		assert p.score == 1;
+	}
+	@Test
+	public void playerEatingIncreasesHealthBy1() {
+		Model m = new Model(1,1,1,1);
+		Player p = new Player();
+		p.health = 1;
+		m.gamePieces.add(p);
+		Food f = new Food(1,1);
+		m.eat(f);
+		assert p.health == 2;
+	}
+		//Cannot write tests for Nest due to graphic nature (It calls a method in view).
 }
