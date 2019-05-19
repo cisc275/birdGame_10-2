b281f5b49274c8376284c3d18dbf79189eeb856b
Nishant Chintala <crnishant@gmail.com>
2019-05-19 16:08:06 -0400

    changed the number of obstacles

--- src/birdgame/initialNumbers.java
+++ src/birdgame/initialNumbers.java
@@ -23,7 +23,7 @@ public class initialNumbers implements Serializable{
 	
 	//Model
 
-	private int obstaclesPerLevel = 3;
+	private int obstaclesPerLevel = 15;
 
 	public int obstaclesPerLevel() {
 		return obstaclesPerLevel;
